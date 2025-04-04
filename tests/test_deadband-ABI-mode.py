9adc9dfb934ee036a746bb44ecd44a6d8a541647
Tim VanHove <t2thev@outlook.com>
2025-04-04 17:24:42 +0000

    moving to ABI mode and adding in gcovr tests.

--- tests/test_deadband-ABI-mode.py
+++ tests/test_deadband-ABI-mode.py
@@ -12,9 +12,9 @@ includes)
 
 C = ffi.dlopen("./deadband.so")
 
-@pytest.mark.parametrize("max",range(0,10)) #250))
-@pytest.mark.parametrize("min",range(0,10)) #250))
-@pytest.mark.parametrize("hysteresis", range(1,3)) #10))
+@pytest.mark.parametrize("max",range(0,250))
+@pytest.mark.parametrize("min",range(0,250))
+@pytest.mark.parametrize("hysteresis", range(1,10))
 def test_deadband_init(max,min,hysteresis):
     teststruct = ffi.new("struct DBND_struct *")
     testOK = C.DBND_initDeadband(teststruct , max, min, hysteresis)
